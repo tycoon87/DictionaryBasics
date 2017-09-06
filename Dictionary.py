4b579f866c524d05580571bbf23877b2c57b881e
tycoon87 <tyson.woodruff@outlook.com>
2017-09-05 19:04:25 -0700

    works

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+questions = {"name":"anna","age":"101","country of birth":"the United States","favorate language":"Python"}
+def qAndA():
+    print "My name is", str(questions["name"])
+    print "My age is", str(questions["age"])
+    print "my countery of birth is", str(questions["country of birth"])
+    print "my favorate language is", str(questions["favorate language"])
+qAndA()
